8335232841f03d591a3bdaab336b31b337a1f204
deadprogram <ron@hybridgroup.com>
2023-05-23 19:32:13 +0200

    Prepare for release 0.3.0
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ version.go
@@ -0,0 +1,7 @@
+package tinyterm
+
+// Version returns a user-readable string showing the version
+// of the TinyTerm package for support purposes.
+//
+// Update this value before release of new version of software.
+const Version = "0.3.0"
